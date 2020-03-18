1afd5aac97804603f1a3d6f28c5cafe3295ff16b
Patrik Matos <patrikmatos.tecinfo@gmail.com>
2020-03-18 11:24:04 -0300

    Controllers

---
+++ src/controllers/listExerciciesB.js
@@ -0,0 +1,132 @@
+module.exports =  function(req, res) {
+  res.json(
+    [
+      {
+        id: 1,
+        type: '',
+        title: 'Leg Press 45',
+        machine: 'maq. 09',
+        colors: '',
+        series: {
+          serie: 3,
+          repetitions: 10,
+        },
+        media: {
+          img: null,
+          video: null
+        }
+      },
+
+      {
+        id: 2,
+        type: '',
+        title: 'Mesa Flexora',
+        machine: 'maq. 10',
+        colors: '',
+        series: {
+          serie: 3,
+          repetitions: 10,
+        },
+        media: {
+          img: null,
+          video: null
+        }
+      },
+
+      {
+        id: 3,
+        type: '',
+        title: 'Stiff Barra',
+        machine: 'Barra Reta',
+        colors: '',
+        series: {
+          serie: 3,
+          repetitions: 10,
+        },
+        media: {
+          img: null,
+          video: null
+        }
+      },
+
+      {
+        id: 4,
+        type: '',
+        title: 'Afundo',
+        machine: 'Halteres',
+        colors: '',
+        series: {
+          serie: 3,
+          repetitions: 12,
+        },
+        media: {
+          img: null,
+          video: null
+        }
+      },      
+
+      {
+        id: 5,
+        type: '',
+        title: 'Cadeira Abdutora ',
+        machine: 'maq. 04',
+        colors: '',
+        series: {
+          serie: 3,
+          repetitions: 15,
+        },
+        media: {
+          img: null,
+          video: null
+        }
+      },
+
+      {
+        id: 6,
+        type: '',
+        title: 'Cadeira Adutora',
+        machine: 'maq. 05',
+        colors: '',
+        series: {
+          serie: 3,
+          repetitions: 15,
+        },
+        media: {
+          img: null,
+          video: null
+        }
+      },
+
+      {
+        id: 7,
+        type: '',
+        title: 'Agachamento',
+        machine: 'Halteres',
+        colors: '',
+        series: {
+          serie: 3,
+          repetitions: 15,
+        },
+        media: {
+          img: null,
+          video: null
+        }
+      },
+      {
+        id: 8,
+        type: '',
+        title: 'Abdominal Twist',
+        machine: 'Bola',
+        colors: '',
+        series: {
+          serie: 3,
+          repetitions: 15,
+        },
+        media: {
+          img: null,
+          video: null
+        }
+      },
+    ]
+  )
+}

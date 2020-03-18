cccd471f8032b54b109091e8b4ca09c97a65f79f
Patrik Matos <patrikmatos.tecinfo@gmail.com>
2020-03-18 10:43:54 -0300

    Controllers

---
+++ src/controllers/listExerciciesA.js
@@ -0,0 +1,101 @@
+module.exports =  function(req, res) {
+  res.json(
+    [
+      {
+        id: 1,
+        type: '',
+        title: 'Puxada máquina',
+        machine: 'maq. 32',
+        colors: '',
+        series: {
+          serie: 3,
+          repetitions: 12,
+        },
+        media: {
+          img: null,
+          video: null
+        }
+      },
+
+      {
+        id: 2,
+        type: '',
+        title: 'Serrátil ( pull down ) corda',
+        machine: 'maq. 62',
+        colors: '',
+        series: {
+          serie: 3,
+          repetitions: 12,
+        },
+        media: {
+          img: null,
+          video: null
+        }
+      },
+
+      {
+        id: 3,
+        type: '',
+        title: 'Remada Curvada',
+        machine: 'maq. 32',
+        colors: '',
+        series: {
+          serie: 3,
+          repetitions: 12,
+        },
+        media: {
+          img: null,
+          video: null
+        }
+      },
+
+      {
+        id: 4,
+        type: '',
+        title: 'Rosca Scott ( barra W )',
+        machine: 'maq. 32',
+        colors: '',
+        series: {
+          serie: 3,
+          repetitions: 12,
+        },
+        media: {
+          img: null,
+          video: null
+        }
+      },      
+
+      {
+        id: 5,
+        type: '',
+        title: 'Rosca Direta ( barra reta ) ',
+        machine: 'maq. 32',
+        colors: '',
+        series: {
+          serie: 3,
+          repetitions: 12,
+        },
+        media: {
+          img: null,
+          video: null
+        }
+      },
+
+      {
+        id: 6,
+        type: '',
+        title: 'Crucifixo Inverso ( Máquina ) ',
+        machine: 'maq. 32',
+        colors: '',
+        series: {
+          serie: 3,
+          repetitions: 12,
+        },
+        media: {
+          img: null,
+          video: null
+        }
+      },
+    ]
+  )
+}

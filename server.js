ba96a43d0d63d2a3eef12e6a7115f437e6861cc5
Patrik <patrikmatos.tecinfo@gmail.com>
2021-05-26 20:59:36 -0300

    wip: refactor code

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // const ctlExercises = require('./src/controllers/exercises');
 const ctlParse = require('./src/controllers/parse');
+const workouts = require('./src/controllers/old___listExercises')
 
 const express = require('express');
 const cors = require('cors');
@@ -17,8 +18,10 @@ app.get('/', (req, res) => {
 // Listar exercicios do parse server
 // app.get('/exercises', ctlExercises.listExercises);
 
+app.get('/v1/workouts', workouts)
+
 // Proxy para o parse server
-app.get('/:className', ctlParse.getClass);
+// app.get('/:className', ctlParse.getClass);
 
 app.listen(3030, function(){
   console.log('App is running on port 3030');
